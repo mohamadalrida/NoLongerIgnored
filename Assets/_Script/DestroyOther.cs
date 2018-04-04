454793ab322bdb51d12c96f01318505475b4f7a3
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-04-04 16:51:28 +1000

    VR Interaction v1.1

---
+++ Assets/_Script/DestroyOther.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyOther : MonoBehaviour {
+
+    public float MyTime = 0f;
+    public Transform MeterProgress;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        MeterProgress.GetComponent<Image>().fillAmount = MyTime;
+
+	}
+	
+	// Update is called once per frame
+	public void Update ()
+    {
+        MyTime += Time.deltaTime;
+
+        MeterProgress.GetComponent<Image>().fillAmount = MyTime/2;
+
+        if (MyTime >= 2f)
+        {
+
+        }
+
+	}
+
+    public void ResetTime()
+    {
+        MyTime = 0f;
+        MeterProgress.GetComponent<Image>().fillAmount = MyTime;
+    }
+}
