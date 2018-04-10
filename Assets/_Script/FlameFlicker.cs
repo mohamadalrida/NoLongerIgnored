9a48b292aea62a19dec72b2ee0e8b44e6d91af6d
blbeavis <brayden.lee.b@gmail.com>
2018-04-10 18:10:48 +1000

    Lantern added
    Lantern and pulsing LIght added

---
+++ Assets/_Script/FlameFlicker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameFlicker : MonoBehaviour {
+
+    private Light fireLight;
+    private Color originalColor;
+    private float timePassed;
+    private float changeValue;
+
+	
+	void Start () {
+        //storing light component to variable
+        fireLight = this.GetComponent<Light>();
+
+
+        if(fireLight != null)
+        {
+            originalColor = fireLight.color;
+        }
+        else
+        {
+            enabled = false;
+            return;
+        }
+
+        changeValue = 0;
+        timePassed = 0;
+	}
+	
+
+	void Update ()
+    {
+        timePassed = Time.time;
+
+        //normalising value to between 0 and 1
+        timePassed = timePassed - Mathf.Floor(timePassed);
+
+        fireLight.color = originalColor * CalculateChange();
+		
+	}
+
+    private float CalculateChange()
+    {
+        //creates fluctuation between frequency, pulse and intensity
+        changeValue = -Mathf.Sin(timePassed * 2 * Mathf.PI) * 0.002f + 0.40f;
+        return changeValue;
+    }
+}
